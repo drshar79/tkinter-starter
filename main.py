c177f15243939f718b824e532d7982460a7d692b
Divyesh Sharma <drshar79>
2020-01-30 09:59:41 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 # A starter program for Python with Tkinter
 
 from tkinter import * # import Tkinter library
-window = Tk()         # Create the application window
-window.title("Sample Text")
-# Add a label with the text "Hello"
-lbl = Label(window, text="Welcome", font=("Times New Roman Bold", 20))
-
-# Place the label in the window at 0, 0
+from tkinter.ttk import*
+from tkinter.ttk import Progressbar
+window = Tk()
+window.title("Sample Text") 
+window.geometry('700x400') 
+lbl = Label(window, text="Welcome", font=("Times New Roman", 50))
 lbl.grid(column=0, row=0)
-window.geometry('350x200')
-btn=Button(window, text="Click Me!", bg="Yellow", fg="Blue")
-btn.grid(column=1, row=0)
+txt=Entry(window, width=10,)
+txt.grid(column=0, row=0)
+txt.focus()
+combo=Combobox(window)
+combo['values']=(1,2,3,4,5, "Text")
+combo.current(1)
+combo.grid(column=3, row=0)
+chk=Checkbutton(window, text='Choose')
+chk.grid(column=0, row=1)
+bar=Progressbar(window, length=200)
+bar['value']=70
+txt.grid(column=1, row=1)
 def clicked():
-    lbl.configure(text="Button was clicked!!")
-btn=Button(window, text="Click Me!", command=clicked)  
-window.mainloop()     # Keep the window open
+    lbl.configure(text="Button was clicked!!", font=("Arial Bold", 20))
+btn = Button(window, text="Click Me", command=clicked)
+btn.grid(column=1, row=0)
+window.mainloop()
