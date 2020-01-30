57dd9be9aca1da1195794748221686448af63fc9
Divyesh Sharma <drshar79>
2020-01-30 11:16:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,15 @@
 from tkinter import * # import Tkinter library
 from tkinter.ttk import*
 from tkinter.ttk import Progressbar
+from tkinter import Menu
+import time
 window = Tk()
 window.title("Sample Text") 
 window.geometry('700x400') 
 lbl = Label(window, text="Welcome", font=("Times New Roman", 50))
 lbl.grid(column=0, row=0)
 txt=Entry(window, width=10,)
-txt.grid(column=0, row=0)
+txt.grid(column=4, row=0)
 txt.focus()
 combo=Combobox(window)
 combo['values']=(1,2,3,4,5, "Text")
@@ -19,7 +21,23 @@ chk=Checkbutton(window, text='Choose')
 chk.grid(column=0, row=1)
 bar=Progressbar(window, length=200)
 bar['value']=70
-txt.grid(column=1, row=1)
+bar.grid(column=1, row=1)
+menu=Menu(window)
+new_item=Menu(menu)
+menu.add_command(label="File")
+new_item.add_command(label="New File")
+new_item.add_command(label="Optional file")
+menu.add_cascade(label="File2", menu=new_item)
+window.config(menu=menu)
+spin=Spinbox(window, from_=0, to=100, width=5)
+spin.grid(column=0, row=2)
+def countdown(n):
+    while n>0:
+        print(n)
+        n=n-1
+    if n==0:
+        print("Time's up!")
+        countdown(50)
 def clicked():
     lbl.configure(text="Button was clicked!!", font=("Arial Bold", 20))
 btn = Button(window, text="Click Me", command=clicked)
